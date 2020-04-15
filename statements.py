10d61b155f67f07abe21faabcfb7bcb269fb4f84
unknown <ravikumar.manam@flex.com>
2020-04-14 23:04:50 -0700

    Statement file uploaded

--- statements.py
+++ statements.py
@@ -1,12 +1,14 @@
+#age = int(input("Enter your age: "))
+name = str(input("Enter your Name: "))
 age = int(input("Enter your age: "))
 #if age >= 14:
-Required_age = 14
-if age == Required_age:
-
-    print("your age is exactly 14")
+if (name[0] == 'a' or name[0] == 'A') and age >= 10:
+    print("You can watch COCO movi")
+elif 5<age<=15:
+    print("You can watch COCO movi with Parents")
 else:
-    if age > Required_age:
-        print("Your age is above 14")
-    else:
-        print("Your age is below 14")
+    #if age > Required_age:
+      #  print("Your age is above 14")
+    #else:
+        print("Sorry you can't watch COCO")
     #pass
